ac4140a9345b566aa65f6f81f8b21a000da5b62a
Arun Prakash Pandey <131158288+D32SSV@users.noreply.github.com>
2023-08-28 10:10:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+//find the reference of the element to be removed
+//find the reference of the button
+//add event listener of select to the button
+//add function to the event listener
+
+let color = document.getElementById("colorSelect");
+
+let btn = document.getElementsByTagName("input")[0].addEventListener("click", function() {
+    const selectedCol = color.value;
+    for (let i = 0; i < color.options.length; i++) {
+        if (selectedCol == color.options[i].value) {
+            color.remove(i);
+            break;
+        }
+    }
+});
